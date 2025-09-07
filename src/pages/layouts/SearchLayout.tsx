03cf61f54962387bb5b3d44a824f92357cc2c64a
Valkyrie <oihanegdcl@gmail.com>
2025-09-07 19:10:40 +0200

    adjustments to mobile version

--- src/pages/layouts/SearchLayout.tsx
+++ src/pages/layouts/SearchLayout.tsx
@@ -8,34 +8,46 @@ export const SearchLayout = () => {
   const toggleAdvancedSearch = () => setShowAdvancedSearch((prev) => !prev);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-amber-50">
-      <div className={`transition-opacity duration-700 ${showAdvancedSearch ? "opacity-35" : "opacity-100"}`}>
-        <SearchBar />
-      </div>
+    <div className="relative flex flex-col items-center justify-center w-full min-h-screen bg-amber-50">
+      {/* Contenedor que engloba todo el bloque y se mueve junto */}
+      <div
+        className={`
+          flex flex-col items-center
+          transition-all duration-700
+          ${showAdvancedSearch ? "lg:-translate-y-1/4" : "translate-y-0"}
+        `}
+      >
+        {/* SearchBar */}
+        <div className={`transition-opacity duration-700 ${showAdvancedSearch ? "sm:opacity-35 opacity-0" : "opacity-100"}`}>
+          <SearchBar />
+        </div>
 
-      <div className="py-4 text-sm text-center">
-        {showAdvancedSearch ? (
-          <>
-            <p>Hide advanced search</p>
-            <button className="cursor-pointer" onClick={toggleAdvancedSearch}>
-              <ChevronUp size={16} />
-            </button>
-          </>
-        ) : (
-          <>
-            <p>Show advanced search</p>
-            <button className="cursor-pointer" onClick={toggleAdvancedSearch}>
-              <ChevronDown size={16} />
-            </button>
-          </>
+        {/* Botón de toggle */}
+        <div className="py-4 text-sm text-center mt-6">
+          {showAdvancedSearch ? (
+            <>
+              <p>Hide advanced search</p>
+              <button className="cursor-pointer" onClick={toggleAdvancedSearch}>
+                <ChevronUp size={16} />
+              </button>
+            </>
+          ) : (
+            <>
+              <p>Show advanced search</p>
+              <button className="cursor-pointer" onClick={toggleAdvancedSearch}>
+                <ChevronDown size={16} />
+              </button>
+            </>
+          )}
+        </div>
+
+        {/* AdvancedSearchForm */}
+        {showAdvancedSearch && (
+          <div className={`absolute top-1/2 -translate-y-1/2 sm:static sm:top-auto sm:translate-y-0 animate-[slideInDownwards_0.5s_ease-in-out_0.1s_both]`}>
+            <AdvancedSearchForm />
+          </div>
         )}
       </div>
-
-      {showAdvancedSearch && (
-        <div className={`animate-[slideInDownwards_0.5s_ease-in-out_0.1s_both]`}>
-          <AdvancedSearchForm />
-        </div>
-      )}
     </div>
   );
 };
